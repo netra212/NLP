a4e9fa5d42535bd528b34e2f5d8a338d440e2452
netra212 <netra200021kcbdr@gmail.com>
2025-06-24 13:36:35 +0100

    RAG

--- RAG/RAG.py
+++ RAG/RAG.py
@@ -6,6 +6,23 @@ from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv, find_dotenv
+import os
+import bs4
 
 # Reading the models.
-api_token = "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"
+# api_key = load_dotenv(find_dotenv(''))
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+# Collecting Data From Internet. 
+WebBaseLoader(
+    web_path = ("http://lilianweng.github.io/posts/2023-06-23-agent/"),
+    bs_kwargs = dict(
+        parse_only = bs4.SoupStrainer(
+            class = ("post-content", "post-title", "post-header")
+        )
+    ),
+
+)
+
+loader.load()
